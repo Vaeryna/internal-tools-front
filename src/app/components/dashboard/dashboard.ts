bd3ba9fe0136e448fa266364eaac5ab396ed052d
Melanie <leblond.melanie@gmail.com>
2026-03-11 23:01:46 +0100

    dashboard kpi

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,47 +1,62 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import {
-  Building2,
-  LucideAngularModule,
-  TrendingDown,
-  TrendingUp,
-  Users,
-  Wrench,
-} from 'lucide-angular';
+import { Building2, LucideAngularModule, TrendingUp, Users, Wrench } from 'lucide-angular';
 import { DashboardServices } from '../../services/dashboard.services';
 import { CurrencyPipe } from '@angular/common';
 import { ShortNumberPipe } from '../../utils/short-number-pipe';
+import { ToolsService } from '../../services/tools.service';
+import { RecentToolsTab } from '../recent-tools-tab/recent-tools-tab';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [LucideAngularModule, CurrencyPipe, ShortNumberPipe],
+  imports: [LucideAngularModule, CurrencyPipe, ShortNumberPipe, RecentToolsTab],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard implements OnInit {
   constructor(
     private dashService: DashboardServices,
+    private toolService: ToolsService,
     private cdr: ChangeDetectorRef,
   ) {}
 
   readonly TrendingUp = TrendingUp;
-  readonly TrendingDown = TrendingDown;
-
   protected readonly Wrench = Wrench;
   protected readonly Building2 = Building2;
   protected readonly Users = Users;
 
   budget: any;
-  positiveBudget: any;
+  trendBudget: any;
+  activeTools: any;
+  trendTools: any;
+  departments: any;
+  trendDepartment: any;
+  costPerUser: any;
+  trendCostPerUser: any;
 
   ngOnInit(): void {
     this.dashService.getAnalytics().subscribe((analytics) => {
-      console.log('ana', analytics);
-      console.log('ana2', analytics.budget_overview);
-
       this.budget = analytics.budget_overview;
-      this.positiveBudget = Number(this.budget.trend_percentage);
-      console.log('budgett', this.budget.trend_percentage);
 
+      this.trendBudget = this.budget.trend_percentage;
+
+      this.trendTools = analytics.kpi_trends.tools_change;
+
+      this.trendDepartment = analytics.kpi_trends.departments_change;
+
+      this.costPerUser = analytics.cost_analytics.cost_per_user;
+
+      this.trendCostPerUser = analytics.kpi_trends.cost_per_user_change;
+
+      this.cdr.detectChanges();
+    });
+
+    this.toolService.getTools({ status: 'active' }).subscribe((tools) => {
+      this.activeTools = tools.length;
+      this.cdr.detectChanges();
+    });
+
+    this.dashService.getTotalDepartment().subscribe((departments) => {
+      this.departments = departments.length;
       this.cdr.detectChanges();
     });
   }
--- src/app/services/dashboard.services.ts
+++ src/app/services/dashboard.services.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
 import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DashboardServices {
-  constructor(
-    private http: HttpClient,
-    private route: ActivatedRoute,
-  ) {}
+  constructor(private http: HttpClient) {}
 
   getAnalytics(): Observable<any> {
     return this.http.get(`api/analytics`).pipe(
       map((analytics) => {
         console.log('service', analytics);
         return analytics;
+      }),
+    );
+  }
+
+
+  getTotalDepartment(): Observable<any> {
+    return this.http.get(`api/departments`).pipe(
+      map((dep) => {
+        return dep;
       }),
     );
   }
