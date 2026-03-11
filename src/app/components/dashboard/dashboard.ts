1df7745e13783081a85f6baff7a9f22469075c52
Melanie <leblond.melanie@gmail.com>
2026-03-11 16:22:24 +0100

    badges card css

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import {
+  Building2,
+  LucideAngularModule,
+  TrendingDown,
+  TrendingUp,
+  Users,
+  Wrench,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {}
+export class Dashboard {
+  readonly TrendingUp = TrendingUp;
+  readonly TrendingDown = TrendingDown;
+  protected positivBudget: boolean = true;
+  protected readonly Wrench = Wrench;
+  protected readonly Building2 = Building2;
+  protected readonly Users = Users;
+}
+//
