0a09161ec1efb8e677c280dfe4dbcb2ea3d562ff
Melanie <leblond.melanie@gmail.com>
2026-03-11 15:07:52 +0100

    Router et Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Tools } from './components/tools/tools';
+import { Analytics } from './components/analytics/analytics';
+import { Settings } from './components/settings/settings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Dashboard,
+  },
+  { path: 'tools', component: Tools },
+  { path: 'analytics', component: Analytics },
+  { path: 'settings', component: Settings },
+];
