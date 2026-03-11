20c9d3adf121ee8531c913c7a59003dfce3b6789
Melanie <leblond.melanie@gmail.com>
2026-03-11 19:44:47 +0100

    card Monthly Budget

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import {
   Building2,
   LucideAngularModule,
@@ -7,19 +7,42 @@ import {
   Users,
   Wrench,
 } from 'lucide-angular';
+import { DashboardServices } from '../../services/dashboard.services';
+import { CurrencyPipe } from '@angular/common';
+import { ShortNumberPipe } from '../../utils/short-number-pipe';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, CurrencyPipe, ShortNumberPipe],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
+  constructor(
+    private dashService: DashboardServices,
+    private cdr: ChangeDetectorRef,
+  ) {}
+
   readonly TrendingUp = TrendingUp;
   readonly TrendingDown = TrendingDown;
-  protected positivBudget: boolean = true;
+
   protected readonly Wrench = Wrench;
   protected readonly Building2 = Building2;
   protected readonly Users = Users;
+
+  budget: any;
+  positiveBudget: any;
+
+  ngOnInit(): void {
+    this.dashService.getAnalytics().subscribe((analytics) => {
+      console.log('ana', analytics);
+      console.log('ana2', analytics.budget_overview);
+
+      this.budget = analytics.budget_overview;
+      this.positiveBudget = Number(this.budget.trend_percentage);
+      console.log('budgett', this.budget.trend_percentage);
+
+      this.cdr.detectChanges();
+    });
+  }
 }
-//
--- src/app/services/dashboard.services.ts
+++ src/app/services/dashboard.services.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class DashboardServices {}
+export class DashboardServices {
+  constructor(
+    private http: HttpClient,
+    private route: ActivatedRoute,
+  ) {}
+
+  getAnalytics(): Observable<any> {
+    return this.http.get(`api/analytics`).pipe(
+      map((analytics) => {
+        console.log('service', analytics);
+        return analytics;
+      }),
+    );
+  }
+}
