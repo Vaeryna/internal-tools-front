da4345a587bc1248e9202f11d6493f84969acf54
Melanie <leblond.melanie@gmail.com>
2026-03-12 13:51:12 +0100

    tableau des tools recents

--- src/app/components/recent-tools-tab/recent-tools-tab.ts
+++ src/app/components/recent-tools-tab/recent-tools-tab.ts
@@ -2,7 +2,8 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ToolsService } from '../../services/tools.service';
 import { Tool } from '../../models/Tool.models';
 import { CapitalizePipe } from '../../utils/capitalize-pipe-pipe';
-import { Calendar, LucideAngularModule } from 'lucide-angular';
+import { sortBy } from '../../utils/sort-by';
+import { Calendar, ChevronDown, ChevronUp, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-recent-tools-tab',
@@ -17,6 +18,10 @@ export class RecentToolsTab implements OnInit {
   ) {}
 
   tools: Tool[] = [];
+  protected readonly Calendar = Calendar;
+  protected readonly ChevronDown = ChevronDown;
+  protected readonly ChevronUp = ChevronUp;
+
 
   ngOnInit() {
     this.toolService
@@ -29,5 +34,7 @@ export class RecentToolsTab implements OnInit {
       });
   }
 
-  protected readonly Calendar = Calendar;
+  sort(filter: string, order: 'asc' | 'des') {
+    return sortBy(filter, order, this.tools);
+  }
 }
