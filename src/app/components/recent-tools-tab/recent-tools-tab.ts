78e3c023d768cccbff8768ae38668d4f7a23a566
Melanie <leblond.melanie@gmail.com>
2026-03-12 11:42:12 +0100

    tableau "recent tool" du dashboard

--- src/app/components/recent-tools-tab/recent-tools-tab.ts
+++ src/app/components/recent-tools-tab/recent-tools-tab.ts
@@ -1,18 +1,17 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { DashboardServices } from '../../services/dashboard.services';
 import { ToolsService } from '../../services/tools.service';
 import { Tool } from '../../models/Tool.models';
 import { CapitalizePipe } from '../../utils/capitalize-pipe-pipe';
+import { Calendar, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-recent-tools-tab',
-  imports: [CapitalizePipe],
+  imports: [CapitalizePipe, LucideAngularModule],
   templateUrl: './recent-tools-tab.html',
   styleUrl: './recent-tools-tab.css',
 })
 export class RecentToolsTab implements OnInit {
   constructor(
-    private dashService: DashboardServices,
     private toolService: ToolsService,
     private cdr: ChangeDetectorRef,
   ) {}
@@ -25,6 +24,10 @@ export class RecentToolsTab implements OnInit {
       .subscribe((tools) => {
         this.tools = tools;
         console.log(tools);
+
+        this.cdr.detectChanges();
       });
   }
+
+  protected readonly Calendar = Calendar;
 }
